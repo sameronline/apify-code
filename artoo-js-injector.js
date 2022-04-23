452d2fd33e73bc432bb86f6ac6c2b10a2983855e
Samer Ali <samer@sameronline.com>
2022-04-24 00:22:16 +0200

    Update artoo-js-injector.js

--- artoo-js-injector.js
+++ artoo-js-injector.js
@@ -2,7 +2,7 @@
     'use strict';
     var artoojsInjector = document.createElement('script');
     artoojsInjector.type = 'text/javascript';
-    artoojsInjector.src = '//cdn.jsdelivr.net/gh/sameronline/apify-code@0.0.2/apify-artoo.js';
+    artoojsInjector.src = '//cdn.jsdelivr.net/gh/sameronline/apify-code@0.0.3/apify-artoo.js';
     artoojsInjector.async = "true";
     artoojsInjector.defer = "true";
     document.head.appendChild(artoojsInjector);
